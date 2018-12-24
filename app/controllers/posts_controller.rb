e3f0301f39d5c15496e32cd0f483c097317fc746
morwen80 <ribesnero80@gmail.com>
2018-12-24 21:16:37 +0000

    Added Configure Permitted Parameters method on ApplicationController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:show, :index]
+#the BEFORE FILTER makes sure that only authenticated user can do certain operations
+#so they have to be signed in unless they are on the index or the show page 
+
 
   # GET /posts
   # GET /posts.json
